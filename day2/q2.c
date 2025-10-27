0259401c14a98dd7b1b9e88e232e5c16c2226b5a
PAVITHRAN S <pavithrand2405@gmail.com>
2025-10-27 21:34:41 +0530

    Add files via upload

---
+++ day2/q2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("character for ASCII value %d=%c",a,a);
+    return 0;
+}
---
+++ day2/q5.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("sizeof char=%d\n",sizeof(char));
+    printf("sizeof int=%d\n",sizeof(int));
+    printf("sizeof float=%d",sizeof(float));
+    return 0;
+}
